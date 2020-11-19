218fb4c0703996bc43a60f385764ecdbf9ba8d2c
Jonatan Schultz <jonatan_schultzz@hotmail.com>
2020-11-19 16:57:59 +0100

    fixes

--- js/customFunctions.js
+++ js/customFunctions.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+//Add element with id="me", give it width: 100%, top: 0, onelementclick will give it a new random color 
 const me = document.getElementById('me');
 function randomColor() {
     return Math.floor(Math.random() * 255);
